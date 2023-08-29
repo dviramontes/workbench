49332647f823618547bdde58bacfb30d41ea278d
David Viramontes <dviramontes@gmail.com>
2023-08-28 23:43:32 -0400

    bench: meetings 1 and 2

---
+++ main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_maxMeetings(t *testing.T) {
+	type args struct {
+		start []int
+		end   []int
+		want  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"A", args{
+			start: []int{1, 3, 0, 5, 8, 5},
+			end:   []int{2, 4, 6, 7, 9, 9},
+		}, []int{1, 2, 4, 5}},
+		//{"B", args{
+		//	start: []int{75250, 50074, 43659, 8931, 11273, 27545, 50879, 77924},
+		//	end:   []int{112960, 114515, 81825, 93424, 54316, 35533, 73383, 160252},
+		//}, []int{6, 7, 1}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := maxMeetings(tt.args.start, tt.args.end)
+			if !reflect.DeepEqual(result, tt.want) {
+				t.Errorf("maxMeetings() = %v, want %v", maxMeetings(tt.args.start, tt.args.end), tt.want)
+			}
+		})
+	}
+}
+
+func Test_canAttendMeetings(t *testing.T) {
+	type args struct {
+		intervals []Interval
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"A", args{
+				intervals: []Interval{{5, 10}, {0, 30}, {15, 20}},
+			}, false,
+		},
+		{
+			"B", args{
+				intervals: []Interval{{5, 8}, {9, 15}},
+			}, true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canAttendMeetings(tt.args.intervals); got != tt.want {
+				t.Errorf("canAttendMeetings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
